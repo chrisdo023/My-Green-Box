60f15bca74ca1f34a8a60cc8014aedc571938e08
Chris Do <do.chris023@gmail.com>
2022-03-23 22:14:55 -0700

    Working on creating APIs for Client

---
+++ flask/__init__.py
@@ -0,0 +1,96 @@
+import datetime
+import sqlite3
+
+from flask import Flask, render_template, g, request
+
+app = Flask(__name__)
+
+# default cabinet.db
+DATABASE = 'cabinet.db'
+database = 'cabinet'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        print("\n\nSuccessfully connected to SQLite\n\n")
+    else:
+        print("Unsuccessfully connected to SQLite")
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.before_first_request
+def create_app():
+    # import db
+    # db.init_db()
+
+    # get_db()
+
+    return app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/post', methods = ['POST', 'GET'])
+def post():
+    if request.method == 'POST':
+        try:
+            con = get_db()
+            cur = con.cursor()
+
+            temperature = request.form["Temperature"]
+            humidity = request.form["Humidity"]
+            createdAt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            cur.execute("INSERT INTO cabinet (id, temperature, humidity, created_at) VALUES (?, ?, ?, ?)", (None, temperature, humidity, createdAt))
+
+            con.commit()
+            con.close()
+            con.close()
+
+            msg = " Temperature and humidity recorded! "
+        except:
+            msg = " Error in recording temperature and humidity :'( "
+        finally:
+            return { "status": msg } 
+
+@app.route('/list', methods = ['GET'])
+def list():
+    if request.method == 'GET':
+        con = sqlite3.connect('cabinet.db')
+        con.row_factory = sqlite3.Row
+
+        cur = con.cursor()
+        cur.execute("select * from cabinet")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row['id'])
+            print(row['temperature'])
+            print(row['humidity'])
+            print(row['created_at'])
+        return render_template("list.html", rows=rows)
+
+@app.route('/retrieve', methods = ['GET'])
+def retrieve():
+    if request.method == 'GET':
+        try:
+            con = sqlite3.connect('cabinet.db')
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+
+            data = cur.execute("select * from cabinet").fetchall()[-1]
+        except:
+            data = []
+        finally:
+            return { "data": data }
+
+if __name__ == "__main__":
+    app.run(debug= True)
--- flask/db.py
+++ flask/db.py
@@ -4,12 +4,11 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+DATABASE = "cabinet.db"
+
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
+        g.db = sqlite3.connect(DATABASE)
         g.db.row_factory = sqlite3.Row
 
     return g.db
@@ -20,19 +19,11 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+# Creating function to run SQL commands in schema.sql
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+        print('\n\nInitialized the database.\n\n')
